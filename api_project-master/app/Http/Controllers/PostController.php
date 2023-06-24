d96499902abeb69693d4fe9386ced59e0a521f41
Ezz-N-Moqayad <ezz.n.moq@gmail.com>
2023-06-24 17:08:21 +0300

    1.0.0

--- api_project-master/app/Http/Controllers/PostController.php
+++ api_project-master/app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Validator;
 class PostController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * تخليني
      */
     public function index()
     {
