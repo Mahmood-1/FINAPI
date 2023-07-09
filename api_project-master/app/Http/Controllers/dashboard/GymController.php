7c0dae77d13bb544061aa7001206831ca4bf5954
Mahmood Shaheen <66056167+Mahmood-1@users.noreply.github.com>
2023-07-09 20:23:06 +0300

    Update GymController.php

--- api_project-master/app/Http/Controllers/dashboard/GymController.php
+++ api_project-master/app/Http/Controllers/dashboard/GymController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\dashboard;
 
 use App\Http\Controllers\Controller;
-use App\Models\gym;
+use App\Models\Gym;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Redirect;
